c60f4659673d9fd3ef578042f2c962011cf03c91
Hanzllasoomro <soomrohanzlla@gmail.com>
2026-01-12 06:58:47 +0500

    project commit

---
+++ backend/migrations/001-create-users.js
@@ -0,0 +1,34 @@
+"use strict";
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("users", {
+      id: {
+        type: Sequelize.BIGINT,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      passwordHash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      role: {
+        type: Sequelize.ENUM("ADMIN", "STUDENT", "TEACHER"),
+      },
+      status: {
+        type: Sequelize.ENUM("ACTIVE", "INACTIVE"),
+        defaultValue: "ACTIVE",
+      },
+      createdAt: Sequelize.DATE,
+      updatedAt: Sequelize.DATE,
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable("users");
+  }
+}
